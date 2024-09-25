dc175df1517b6fbc465fc593b3d625673bdb9020
thi899 <thiagosimao74@gmail.com>
2024-09-25 02:11:46 -0300

    refactor(): refactor delivery-list component

--- src/app/pages/delivery-list/delivery-list.component.ts
+++ src/app/pages/delivery-list/delivery-list.component.ts
@@ -2,25 +2,30 @@ import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@ang
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subject, takeUntil } from 'rxjs';
+import { DeliveriesDataSourceService } from 'src/app/services/deliveries-data-source.service';
 import { Deliveries, Delivery } from 'src/app/services/model/delivery.model';
 import { DeliveriesStateService } from 'src/app/services/state/deliveries.state.service';
 
 @Component({
   selector: 'app-delivery-list',
   templateUrl: './delivery-list.component.html',
-  styleUrl: './delivery-list.component.scss'
+  styleUrls: ['./delivery-list.component.scss']
 })
 export class DeliveryListComponent implements AfterViewInit, OnDestroy {
   @ViewChild('paginator') paginator: MatPaginator;
   @ViewChild('inputFilter') inputFilter: ElementRef;
 
+  dataSource: MatTableDataSource<Delivery>;
+
   private destroy$ = new Subject<void>();
 
   deliveries: Deliveries;
   displayedColumns: string[];
-  dataSource: MatTableDataSource<Delivery>;
 
-  constructor(private state: DeliveriesStateService) { }
+  constructor(
+    private state: DeliveriesStateService,
+    protected deliveryDataSource: DeliveriesDataSourceService
+  ) { }
 
   ngAfterViewInit(): void {
     this.getDeliveries();
@@ -32,47 +37,24 @@ export class DeliveryListComponent implements AfterViewInit, OnDestroy {
   }
 
   getDeliveries(): void {
-    this.state
-      .getDeliveries()
+    this.state.getDeliveries()
       .pipe(takeUntil(this.destroy$))
       .subscribe((deliveries: Deliveries) => {
         this.deliveries = deliveries;
         this.displayedColumns = Object?.keys(this.deliveries[0]);
-        this.dataSource = new MatTableDataSource(this.deliveries);
-        this.dataSource.paginator = this.paginator;
+        this.deliveryDataSource.setData(this.deliveries);
+        this.deliveryDataSource.setPaginator(this.paginator);
+        this.dataSource = this.deliveryDataSource.getDataSource;
+        this.dataSource.filterPredicate = this.deliveryDataSource.getFilterPredicate();
       });
-
   }
 
   applyFilter(value: string) {
     const filterValue = this.cleanInput(value);
-
-    this.dataSource.filterPredicate = (data: Delivery, filter: string) => {
-      return this.matchesFilter(data.id, filter) ||
-        this.matchesFilter(data.documento, filter) ||
-        this.matchesFilter(data.motorista?.nome, filter) ||
-        this.matchesFilter(data.cliente_origem?.nome, filter) ||
-        this.matchesFilter(data.cliente_origem?.endereco, filter) ||
-        this.matchesFilter(data.cliente_origem?.cidade, filter) ||
-        this.matchesFilter(data.cliente_destino?.nome, filter) ||
-        this.matchesFilter(data.cliente_destino?.endereco, filter) ||
-        this.matchesFilter(data.cliente_destino?.cidade, filter) ||
-        this.matchesFilter(data.status_entrega, filter);
-    };
-
-    this.dataSource.filter = filterValue;
+    this.deliveryDataSource.applyFilter(filterValue);
   }
 
   private cleanInput(value: string): string {
     return value.trim().toLowerCase().normalize("NFD").replace(/[^\w\s]/gi, '').replace(/[\u0300-\u036f]/g, '');
   }
-
-  private matchesFilter(field: string | undefined, filter: string): boolean {
-    if (!field) return false;
-
-    const normalizedField = field.normalize("NFD").replace(/[\u0300-\u036f]/g, '').toLowerCase();
-    return normalizedField.includes(filter);
-  }
-
-
 }
---
+++ src/app/services/deliveries-data-source.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Delivery } from 'src/app/services/model/delivery.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class DeliveriesDataSourceService {
+    private dataSource: MatTableDataSource<Delivery>;
+
+    constructor() {
+        // Inicializando com um array vazio para evitar undefined
+        this.dataSource = new MatTableDataSource<Delivery>([]);
+    }
+
+    get getDataSource(): MatTableDataSource<Delivery> {
+        return this.dataSource;
+    }
+
+    setData(deliveries: Delivery[]) {
+        this.dataSource.data = deliveries;
+    }
+
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    setPaginator(paginator: MatPaginator) {
+        this.dataSource.paginator = paginator;
+    }
+
+    getFilterPredicate() {
+        return (data: Delivery, filter: string): boolean => {
+            const normalizedFilter = filter.normalize("NFD").replace(/[\u0300-\u036f]/g, '').toLowerCase();
+
+            return this.matchesFilter(data.id.toString(), normalizedFilter) ||
+                this.matchesFilter(data.documento, normalizedFilter) ||
+                this.matchesFilter(data.motorista?.nome, normalizedFilter) ||
+                this.matchesFilter(data.cliente_origem?.nome, normalizedFilter) ||
+                this.matchesFilter(data.cliente_origem?.endereco, normalizedFilter) ||
+                this.matchesFilter(data.cliente_origem?.cidade, normalizedFilter) ||
+                this.matchesFilter(data.cliente_destino?.nome, normalizedFilter) ||
+                this.matchesFilter(data.cliente_destino?.endereco, normalizedFilter) ||
+                this.matchesFilter(data.cliente_destino?.cidade, normalizedFilter) ||
+                this.matchesFilter(data.status_entrega, normalizedFilter);
+        };
+    }
+
+    private matchesFilter(field: string | undefined, filter: string): boolean {
+        if (!field) return false;
+
+        const normalizedField = field.normalize("NFD").replace(/[\u0300-\u036f]/g, '').toLowerCase();
+        return normalizedField.includes(filter);
+    }
+}
